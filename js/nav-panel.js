5f20871a5a67b87029a36c6ac850764b70f6ae7a
VasyaYovbak <vasa23102003@gmail.com>
2021-12-14 21:09:51 +0200

    fixed some bugs

--- js/nav-panel.js
+++ js/nav-panel.js
@@ -2,7 +2,6 @@ class NavPanel {
     toggleNav() {
         document.body.classList.toggle("nav_open");
     }
-
     addListenerToLanguage() {
         const language = document.querySelector(".lang_menu");
         const lang_list = document.querySelector("ul");
@@ -10,6 +9,9 @@ class NavPanel {
             lang_list.classList.toggle("visible");
         })
         document.querySelector("main a").addEventListener("click",()=>{
+            this.toggleNav();
+        })
+        document.querySelector("nav.nav a.nav_close").addEventListener("click",()=>{
             this.toggleNav();
         })
     }
